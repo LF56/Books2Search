98b6171e6af972204a92d9f3e43d145daf5736d0
LF56 <leah.fox7@gmail.com>
2022-05-16 08:14:45 -0400

    added save book mutation and runs on graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,35 +3,43 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String
-    email: String
+    username: String!
+    email: String!
     bookCount: Int
-    savedBooks: [Thought]
-    friends: [User]
+    savedBooks: [Book]
   }
 
   type Book {
     bookId: String!
     authors: [String]
     description: String
-    title: String!
+    title: String
     image: String
     link: String
   }
-
+  input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+  
   type Auth {
     token: ID!
     user: User
   }
-
+  
   type Query {
     me: User
   }
-
+  
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook()
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
